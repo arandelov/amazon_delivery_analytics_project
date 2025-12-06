d986e10267fd386d00ef5499bd8ae9e3d8e84365
arandelov <arandelov@users.noreply.github.com>
2025-12-06 23:58:54 +0100

    Merge branch 'main' of https://github.com/arandelov/amazon_delivery_analytics_project

--- 01_database_setup.sql
+++ 01_database_setup.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- Create a database
 CREATE DATABASE IF NOT EXISTS PortfolioProject;
 USE PortfolioProject;
@@ -27,34 +26,4 @@ CREATE TABLE IF NOT EXISTS agents (
     Agent_Age INT NULL,
     Agent_Rating DECIMAL(2,1) NULL,
     FOREIGN KEY (Order_ID) REFERENCES orders(Order_ID)
-=======
--- Create a database
-CREATE DATABASE IF NOT EXISTS PortfolioProject;
-USE PortfolioProject;
-
--- Create orders table
-CREATE TABLE IF NOT EXISTS orders (
-    Order_ID VARCHAR(50) PRIMARY KEY,
-    Store_Latitude DOUBLE NULL,
-    Store_Longitude DOUBLE NULL,
-    Drop_Latitude DOUBLE NULL,
-    Drop_Longitude DOUBLE NULL,
-    Order_Date DATE NULL,
-    Order_Time TIME NULL,
-    Pickup_Time TIME NULL,
-    Area VARCHAR(50) NULL,
-    Category VARCHAR(50) NULL,
-    Delivery_Time INT NULL,
-    Weather VARCHAR(50) NULL,
-    Traffic VARCHAR(50) NULL,
-    Vehicle VARCHAR(50) NULL
-);
-
--- Create agents table
-CREATE TABLE IF NOT EXISTS agents (
-    Order_ID VARCHAR(50) PRIMARY KEY,
-    Agent_Age INT NULL,
-    Agent_Rating DECIMAL(2,1) NULL,
-    FOREIGN KEY (Order_ID) REFERENCES orders(Order_ID)
->>>>>>> 99ff1e19e64620614a5cf0c07ebc341c8728f265
 );
